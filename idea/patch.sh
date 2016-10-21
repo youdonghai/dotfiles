088f3231abfe62237827f8b190adefefd1266aec
You Donghai <youd@garena.com>
2016-10-21 14:58:48 +0700

    update jetbrains terminal settings

--- idea/patch.sh
+++ idea/patch.sh
@@ -7,7 +7,7 @@ fi
 
 path_to_ide=$1
 
-jar uf ${path_to_ide}/lib/pycharm.jar com/intellij/openapi/keymap/impl/*
+jar uf ${path_to_ide}/lib/idea.jar com/intellij/openapi/keymap/impl/*
 mv terminal_old org
 jar uf ${path_to_ide}/plugins/terminal/lib/terminal.jar org/jetbrains/plugins/terminal/*
 mv org terminal_old
