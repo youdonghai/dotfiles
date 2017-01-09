4e3cdba5022d5978f73daa6e16ce7c9dc95c88df
You Donghai <youdonghai@gmail.com>
2017-01-09 17:44:22 +0700

    update jetbrains patch

--- idea/patch.sh
+++ idea/patch.sh
@@ -8,9 +8,7 @@ fi
 path_to_ide=$1
 
 jar uf ${path_to_ide}/lib/idea.jar com/intellij/openapi/keymap/impl/*
-mv terminal_old org
 jar uf ${path_to_ide}/plugins/terminal/lib/terminal.jar org/jetbrains/plugins/terminal/*
-mv org terminal_old
 
 grep 'emitFirstKeystrokeWhenTimeout' ${path_to_ide}/bin/idea.properties
 OUT=$?
